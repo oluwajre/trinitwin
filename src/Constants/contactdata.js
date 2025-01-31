b804d70007300d91a741cf335337a0858864cae4
Ajayi Adeoluwa <oluwajre2412@gmail.com>
2025-01-31 09:18:40 +0100

    Changed the Whatsapp Number

--- src/Constants/contactdata.js
+++ src/Constants/contactdata.js
@@ -4,7 +4,7 @@ export const contactdata = {
     address: 'Suite 12, Winner shopping complex, opposite mobile filling station, Isihor, Edo, Nigeria',
     instagram: 'https://www.instagram.com/trinitwin_energy/',
     facebook: 'https://www.facebook.com/profile.php?id=100063938056686',
-    whatsapp: 'https://api.whatsapp.com/send/?phone=2348032001784&text&type=phone_number&app_absent=0',
+    whatsapp: 'https://api.whatsapp.com/send/?phone=2348099888444&text&type=phone_number&app_absent=0',
     youtube_hero: 'https://www.youtube.com/embed/1kUE0BZtTRc?si=mQnGNZCUQtWYKY-H"',
     youtube_presentation: 'https://www.youtube.com/embed/1kUE0BZtTRc?si=mQnGNZCUQtWYKY-H"',
     linkedin: 'https://www.linkedin.com/in/anthony-abereoje-08215639/',
